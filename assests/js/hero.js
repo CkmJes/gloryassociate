75490dd17b4678066ddea9d70e66e2d9b23b9617
Glory Group Digital Marketing Solution <ckm.jesu8426@gmail.com>
2025-10-31 03:23:15 -0700

    Add files via upload

---
+++ assests/js/hero.js
@@ -0,0 +1,8 @@
+// document.querySelectorAll(".service-card").forEach(card => {
+//   card.addEventListener("mouseenter", () => {
+//     card.style.transform = "translateY(-6px)";
+//   });
+//   card.addEventListener("mouseleave", () => {
+//     card.style.transform = "translateY(0)";
+//   });
+// });
